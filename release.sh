1dda1475cd61f69a282cb2d1cc9211b1bd1659ab
Alex Kasimov <al4str.ftw@gmail.com>
2018-08-26 18:40:09 +0500

    update release script

--- release.sh
+++ release.sh
@@ -3,12 +3,7 @@
 if [[ `git status --porcelain --untracked-files=no` ]]; then
 	echo "You have uncommitted changes"
 else
-	rm -rf ./dist/*
-	cp -r ./src/* ./dist
-	rm -rf ./dist/styles/*
-	npx sass --no-source-map -s compressed -c ./dist/scss:./dist/styles
-	npx postcss ./dist/styles/*.css -u autoprefixer -r --verbose --no-map
-	rm -rf ./dist/scss
+	/bin/bash ./generate.sh
 	git commit --amend
 	git pull
 	git push
