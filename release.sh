51289267fae70a586a5736405c43d1f2e6666332
Alex Kasimov <al4str.ftw@gmail.com>
2018-08-26 18:41:21 +0500

    comment dist generation

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@
 if [[ `git status --porcelain --untracked-files=no` ]]; then
 	echo "You have uncommitted changes"
 else
-	/bin/bash ./generate.sh
-	git commit --amend
+	#/bin/bash ./generate.sh
+	#git commit --amend
 	git pull
 	git push
 fi
