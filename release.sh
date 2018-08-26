10a01add46f5ad4e742bf529e3a1a159b852d2b2
Alex Kasimov <al4str.ftw@gmail.com>
2018-08-26 16:56:23 +0500

    update release script

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#for filename in "$@"; do
-#done
-npx sass --no-source-map -s compressed -c ./src/scss:./dist/styles
+rm -rf ./dist/*
+cp -r ./src/* ./dist
+rm -rf ./dist/styles/*
+npx sass --no-source-map -s compressed -c ./dist/scss:./dist/styles
 npx postcss ./dist/styles/*.css -u autoprefixer -r --verbose --no-map
+rm -rf ./dist/scss
+
