ba35b877e06dcb536625acb0600b2f11ca670ec3
Alex Kasimov <al4str.ftw@gmail.com>
2018-08-26 17:54:06 +0500

    update release script

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ else
 	npx sass --no-source-map -s compressed -c ./dist/scss:./dist/styles
 	npx postcss ./dist/styles/*.css -u autoprefixer -r --verbose --no-map
 	rm -rf ./dist/scss
+	git commit --amend
 	git pull
 	git push
 fi
