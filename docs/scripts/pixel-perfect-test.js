6d0481b605330e38d1271d83a0560e06ac4abb12
Alex Kasimov <al4str.ftw@gmail.com>
2018-08-28 17:34:33 +0500

    add github pages directory

---
+++ docs/scripts/pixel-perfect-test.js
@@ -0,0 +1,34 @@
+
+window.onload = () => {
+  const $cover = document.getElementById('cover');
+  const $switch = document.getElementById('switch');
+  const $opacity = document.getElementById('opacity');
+  if (!$cover || $switch || $opacity || window.Vue === undefined) {
+    return;
+  }
+  new Vue({
+    data: {
+      isShown: $switch.checked,
+    },
+    created() {
+      this.setVisibility(this.isShown);
+      this.opacity();
+      $switch.onclick = this.toggle.bind(this);
+      $opacity.oninput = this.opacity.bind(this);
+    },
+    methods: {
+      opacity() {
+        $cover.style.opacity = ($opacity.value / 100).toString();
+      },
+      setVisibility(visibility) {
+        this.isShown = visibility;
+        $switch.checked = visibility;
+        $cover.hidden = !visibility;
+        $opacity.disabled = !visibility;
+      },
+      toggle() {
+        this.setVisibility(!this.isShown);
+      },
+    },
+  });
+};
