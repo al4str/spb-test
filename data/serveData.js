96157cfb6c56c6e9c7ec8d058d995ac65c72ad70
Alex Kasimov <al4str.ftw@gmail.com>
2018-08-31 19:01:49 +0500

    update data obtaining on github docs

--- data/serveData.js
+++ data/serveData.js
@@ -3,16 +3,19 @@
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const util = require('util');
 
+const { promisify } = util;
+const readFile = promisify(fs.readFile);
 const server = http.createServer();
 
 server.on('request', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   const filePath = path.resolve(__dirname, 'data.json');
-  const stream = fs.createReadStream(filePath);
-  stream.on('end', () => res.end());
-  stream.pipe(res);
+  const data = await readFile(filePath, 'utf-8');
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(data);
 });
 
 server.listen(5005, () => console.info('localhost:5005'));
